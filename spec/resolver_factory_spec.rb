5c7f77a7b6f1f9342bec95c964ae5142da0f65f0
Aaron L <aaron@bettercoder.net>
2012-07-22 17:51:05 +0900

    Makes the resolver spec useful.

--- spec/resolver_factory_spec.rb
+++ spec/resolver_factory_spec.rb
@@ -2,7 +2,7 @@ require_relative '../src/resolver_factory'
 
 describe "ResolverFactory" do
   it "should resolve ip addresses to hostnames" do
-    ResolverFactory.resolve '64.31.0.226'
+    ResolverFactory.resolve('64.31.0.226').should match(/([a-z0-9]+\.)+[a-z]{2,}/i)
   end
 end
 
